a263add33514d4348c0d313705115101cad80573
chuanjieguo@139.com <Gmcc@123>
2022-04-05 18:24:53 +0800

    First Commit

--- Cosine.py
+++ Cosine.py
@@ -1,6 +1,7 @@
 # author: Chuanjie Guo
 # contact: chuanjieguo@139.com
 
+
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import CountVectorizer
 cv = CountVectorizer()
